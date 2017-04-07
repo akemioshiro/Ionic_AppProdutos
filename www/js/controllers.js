2a99d433ed7a5202d6fa6ed77be690470cdc2845
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2017-04-06 22:41:32 -0300

    Listagem, edição e adição de registros

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
 
-.controller('ProdutoController', function($scope) {
+.controller('ProdutoController', function($scope, $ionicModal) {
   $scope.produtos = [
     { titulo: 'Livro de IONIC', valor: 50.99, publicar:true },
     { titulo: 'Livro de Angular JS', valor: 45.99, publicar:true },
@@ -10,5 +10,57 @@ angular.module('starter.controllers', [])
     { titulo: 'Livro de Android', valor: 50.99, publicar:true },
     { titulo: 'Livro de SQL', valor: 20.00, publicar:true }
   ];
-})
 
+  $ionicModal.fromTemplateUrl('templates/adicionar.html', {
+  	scope: $scope
+  }).then(function(modal){
+  	$scope.modal = modal;
+  });
+
+  $scope.abreAdicionar = function()
+  {
+  	$scope.editando = false;
+  	$scope.Produto.titulo = "";
+  	$scope.Produto.valor = "";
+  	$scope.Produto.publicar = false;
+  	$scope.modal.show();
+  }
+
+  $scope.addProduto = function(Produto)
+  {
+  	$scope.produtos.push({
+  		titulo: Produto.titulo, valor: Produto.valor, publicar:Produto.publicar
+  	});
+  	Produto.titulo = "";
+  	Produto.valor = "";
+  	Produto.publicar = false;
+  	$scope.modal.hide();
+  };
+
+  $scope.Produto = {};
+
+  $scope.editando = false;
+
+  var auxProdutoEditar;
+
+  $scope.editarProduto = function(ProdutoEditar)
+  {
+  	$scope.editando = true;
+  	$scope.Produto.titulo = ProdutoEditar.titulo;
+  	$scope.Produto.valor = ProdutoEditar.valor;
+  	$scope.Produto.publicar = ProdutoEditar.publicar;
+  	auxProdutoEditar = ProdutoEditar;
+  	$scope.modal.show();
+  };
+
+  $scope.salvarProduto = function(Produto)
+  {
+  	auxProdutoEditar.titulo = Produto.titulo;
+  	auxProdutoEditar.valor = Produto.valor;
+  	auxProdutoEditar.publicar = Produto.publicar;
+  	$scope.modal.hide();
+  };
+
+
+})
+	
