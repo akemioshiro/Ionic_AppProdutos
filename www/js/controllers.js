748612c272d98f0a871ea9e98f19c441707bfbde
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2017-04-06 23:25:44 -0300

    Validação

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 
 
-.controller('ProdutoController', function($scope,$ionicModal) {
+.controller('ProdutoController', function($scope,$ionicModal,$ionicPopup) {
   $scope.produtos = [
     { titulo: 'Livro de Ionic', valor: 50.99, publicar:true },
     { titulo: 'Livro de Angular JS', valor: 40.99, publicar:true },
@@ -24,6 +24,10 @@ angular.module('starter.controllers', [])
   }
 
   $scope.addProduto = function(Produto){
+  	if(!validacao(Produto))
+  	{
+  		return;
+  	}
   	$scope.produtos.push({titulo: Produto.titulo, valor: Produto.valor, publicar:Produto.publicar});
   	Produto.titulo = "";
   	Produto.valor = "";
@@ -31,11 +35,12 @@ angular.module('starter.controllers', [])
   	$scope.modal.hide();
   }
 
-  $scope.Produto = {};
+  $scope.Produto = {titulo:"", valor:"", publicar:false};
   $scope.editando = false;
   var auxProdutoEditar;
 
   $scope.editarProduto = function(ProdutoEditar){
+
   	$scope.editando = true;
   	$scope.Produto.titulo = ProdutoEditar.titulo;
   	$scope.Produto.valor = ProdutoEditar.valor;
@@ -45,6 +50,10 @@ angular.module('starter.controllers', [])
   };
 
   $scope.salvarProduto = function(Produto){
+  	if(!validacao(Produto))
+  	{
+  		return;
+  	}
   	auxProdutoEditar.titulo = Produto.titulo;
   	auxProdutoEditar.valor = Produto.valor;
   	auxProdutoEditar.publicar = Produto.publicar;
@@ -61,6 +70,32 @@ angular.module('starter.controllers', [])
   };
 
   $scope.deletando = false;
+
+  var validacao = function(Produto){
+  	var textoErro = "";
+  	var existeErro=false;
+
+  	if(Produto.titulo ===''){
+  		existeErro = true;
+  		textoErro += "<p>Preencha um titulo válido</p>";
+  	}
+	if(Produto.valor ===''){
+  		existeErro = true;
+  		textoErro += "<p>Preencha um valor válido</p>";
+  	}
+  	if(existeErro){
+	   	var alertPopup= $ionicPopup.alert({
+	  		title:"Erro no formulário",
+	  		template: textoErro
+	  	});
+	  	return false; 		
+  	}
+  	else{
+  		return true;
+  	}
+
+
+  };
 
 
 })
